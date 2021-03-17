d5f3317a2e6457362576bf76db954df43ac1f1c9
Stoian Mihai <mihai_stoian21@yahoo.com>
2021-03-17 22:21:00 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import videoRoutes from './routes/videos.js';
@@ -42,3 +43,4 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+mongoose.set('useFindAndModify', false);

46ac63a0b2b192030fe400da54f0ef76e4930175
Stoian Mihai <mihai_stoian21@yahoo.com>
2021-03-18 11:36:57 +0200

    content type

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,21 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 app.use(express.json())
-app.use(cors());
+
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+app.use(allowCrossDomain);
 
 //to see what we are requisting
 app.use((req, res, next) => {
